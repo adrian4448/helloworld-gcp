c51d68e3211e05ae911ebe5df586bd3918a5128c
adrian4448 <adrian.morais4448@gmail.com>
2022-02-05 20:57:08 -0300

    Teste da pipeline, deploy e montagem do container

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    Teste do adrian zimboide
+    Commit novo para teste GCP - Sabrina Fedida
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
