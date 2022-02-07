19d1f5e435cbab41f97fe241be2526369fcede7f
adrian4448 <adrian.morais4448@gmail.com>
2022-02-06 23:31:37 -0300

    Ajust texto - teste

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    Commit novo para teste GCP - Sabrina Fedida 2
+    Commit novo para teste GCP
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
