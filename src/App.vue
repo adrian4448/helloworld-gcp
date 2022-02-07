d554a7cc13198f729fb0d4d7acf7f2e60be32f86
adrian4448 <adrian.morais4448@gmail.com>
2022-02-07 18:07:14 -0300

    teste pipeline

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    Commit novo para teste GCP
+    General zambia
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
